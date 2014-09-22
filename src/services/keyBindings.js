3600dc30134a9e616ccdadf3ff2130915da06a6b
mvbschot <m.vbschot@gmail.com>
2014-09-22 18:25:37 +0200

    docs(keyBindings): add ngdoc for all methods

--- src/services/keyBindings.js
+++ src/services/keyBindings.js
@@ -3,6 +3,8 @@ angular.module('voorhoede.components.keyBindings.services.keyBindings', [])
  * @ngdoc service
  * @name keyBindings
  * @module voorhoede.components.keyBindings
+ * @description
+ * Manages the grouping of key combos and handlers, and (un)binds combos at appropriate moments using Mousetrap.
  */
     .service('keyBindings', function() {
         var self = this;
@@ -11,19 +13,28 @@ angular.module('voorhoede.components.keyBindings.services.keyBindings', [])
          * @ngdoc property
          * @name keyBindings#comboGroups
          * @type {object}
+         * @description
+         * Contains all currently active handlers, grouped by key combo.
          */
-        self.comboGroups = {}; // TODO: use this for a cheat sheet component
+        // TODO: use this for a cheat sheet component
+        self.comboGroups = {};
 
         /**
          * @ngdoc method
          * @name keyBindings#addHandler
+         *
          * @param handler {object}
-         * Should contain:
+         * Handler object that should contain at least:
          *
-         * - __combo__: Combo that will be passed to Mousetrap
-         * - __handler__: Function that will be used as event handler
+         * - __combo__: A key combo that will be passed to Mousetrap.
+         *   [List of supported keys](http://craig.is/killing/mice#keys)
+         * - __handler__: Function that will be used as event handler. It will be passed an `event` object
+         *   and the `combo` that triggered the event.
          *
-         * @returns {function} Function to remove the handler
+         * You can add any additional properties that you may want to use later (e.g., you can pass a `description`
+         * if you want to be able to generate a human-readable cheat sheet of active key bindings.)
+         *
+         * @returns {function} Function to remove the handler.
          */
         self.addHandler = function(handler) {
 
@@ -55,16 +66,43 @@ angular.module('voorhoede.components.keyBindings.services.keyBindings', [])
             };
         };
 
+        /**
+         * @ngdoc method
+         * @name keyBindings#bindCombo
+         * @description
+         * Binds {@link keyBindings#triggerHandlers triggerHandlers()} to the specified key combo using Mousetrap.
+         *
+         * @param combo {string}
+         */
         self.bindCombo = function(combo) {
             Mousetrap.bind(combo, function(event, _combo) {
                 self.triggerHandlers(self.comboGroups[combo], event, _combo);
             });
         };
 
+        /**
+         * @ngdoc method
+         * @name keyBindings#unbindCombo
+         * @description
+         * Unbinds the specified key combo using Mousetrap.
+         *
+         * @param combo {string}
+         */
         self.unbindCombo = function(combo) {
             Mousetrap.unbind(combo);
         };
 
+        /**
+         * @ngdoc method
+         * @name keyBindings#triggerHandlers
+         * @description
+         * Triggers the appropriate handlers in the provided group. This means in any case the last one,
+         * and the next handler for each handler that returns `true`.
+         *
+         * @param handlers {array}
+         * @param event {object}
+         * @param combo {string}
+         */
         self.triggerHandlers = function(handlers, event, combo) {
             var triggerNext = true;
             var i = handlers.length;
