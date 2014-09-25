04eb1d2e9ccf6e521b3e046ffae21a2388ec8a50
Jasper Moelker <j.b.moelker@activeids.nl>
2014-09-25 14:12:19 +0200

    chore: style gulpfile to order deps, config, task registration & task functions A-Z, and so that task registrations are always one liners
     Inspired by 'alphabetized one-liners' in John Papa's AngularJS Style Guide https://github.com/johnpapa/angularjs-styleguide

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
+/* Dependencies (A-Z) */
+var concat = require('gulp-concat');
+var Dgeni = require('dgeni');
 var gulp = require('gulp');
 var karma = require('gulp-karma');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rimraf = require('gulp-rimraf');
+var pkg = require('./package.json');
 var rename = require('gulp-rename');
-var Dgeni = require('dgeni');
+var rimraf = require('gulp-rimraf');
+var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
 
-var concatName = 'angular-key-bindings.js';
+/* Shared configuration (A-Z) */
+var concatName = pkg.name + '.js';
 
 var path = {
     src: ['src/**/*.js'],
@@ -36,39 +39,54 @@ path.test = [
     'test/**/*.spec.js'
 ].concat(path.src);
 
+/* Register default & custom tasks (A-Z) */
 gulp.task('default', ['build_source']);
+gulp.task('build_docs', ['build_docs_assets', 'build_docs_dgeni']);
+gulp.task('build_docs_assets', ['build_source'], buildDocsAssetsTask);
+gulp.task('build_docs_dgeni', buildDocsDgeniTask);
+gulp.task('build_source', buildSourceTask);
+gulp.task('rimraf', rimrafTask(path.output));
+gulp.task('rimraf_docs', rimrafTask(path.outputDocs));
+gulp.task('serve_docs', serveDocsTask);
+gulp.task('test_run', testTask({action: 'run', configFile: 'karma.conf.js'}));
+gulp.task('test_watch', testTask({action: 'watch', configFile: 'karma.conf.js'}));
 
-gulp.task('build_source', function() {
+/* Tasks and utils (A-Z) */
+
+function buildDocsAssetsTask() {
+    return gulp.src(path.docsAssets)
+        .pipe(gulp.dest(path.outputDocs + '/assets'))
+}
+
+function buildDocsDgeniTask() {
+    var dgeni = new Dgeni([require('./docs/config')]);
+    return dgeni.generate().catch(function(error) {
+        process.exit(1);
+    });
+}
+
+function buildSourceTask() {
     return gulp.src(path.src)
         .pipe(concat(concatName))
         .pipe(gulp.dest(path.output))
         .pipe(uglify())
         .pipe(rename({extname: '.min.js'}))
         .pipe(gulp.dest(path.output));
-});
+}
 
-gulp.task('build_docs', ['build_docs_assets', 'build_docs_dgeni']);
+function rimrafTask(src) {
+    return function(){
+        gulp.src(src, {read: false})
+            .pipe(rimraf());
+    }
+}
 
-gulp.task('build_docs_dgeni', function() {
-    var dgeni = new Dgeni([require('./docs/config')]);
-    return dgeni.generate().catch(function(error) {
-        process.exit(1);
-    });
-});
-gulp.task('build_docs_assets', ['build_source'], function() {
-    return gulp.src(path.docsAssets)
-        .pipe(gulp.dest(path.outputDocs + '/assets'))
-});
-
-gulp.task('rimraf', function() {
-    return gulp.src(path.output, {read: false}).pipe(rimraf());
-});
-gulp.task('rimraf_docs', function() {
-    return gulp.src(path.outputDocs, {read: false}).pipe(rimraf());
-});
-
-gulp.task('test_run', testTask({action: 'run', configFile: 'karma.conf.js'}));
-gulp.task('test_watch', testTask({action: 'watch', configFile: 'karma.conf.js'}));
+function serveDocsTask() {
+    gulp.src('dist/docs')
+        .pipe(webserver({
+            open: true
+        }));
+}
 
 function testTask(karmaConfig) {
     return function() {
@@ -79,10 +97,3 @@ function testTask(karmaConfig) {
             });
     }
 }
-
-gulp.task('serve_docs', function() {
-    gulp.src('dist/docs')
-        .pipe(webserver({
-            open: true
-        }));
-});
